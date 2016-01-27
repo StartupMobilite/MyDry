875dc8212b383502c9c404022075f2b2af57359c
bachUp <anne-sophie.bach@outlook.com>
2016-01-27 14:05:12 +0100

    mise a niveau

---
+++ Lemon/Lemon/Lemon.Droid/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+using Xamarin.Forms.Platform.Android;
+
+namespace Lemon.Droid
+{
+    [Activity(Label = "HelloXamarinFormsWorld",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+    public class MainActivity : FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Xamarin.Forms.Forms.Init(this, bundle);
+
+            LoadApplication(new App());
+        }
+    }
+}
+
+
---
+++ Lemon/Lemon/Lemon.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Xamarin.Forms;
+using System;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace Lemon.WinPhone
+{
+    
+
+    public sealed partial class MainPage 
+ //   public partial class MainPage : Xamarin.Forms.Platform.WinPhone.FormsApplicationPage // superclass new in 1.3
+
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+          //  SupportedOrientations = SupportedPageOrientation.PortraitOrLandscape;
+
+            //global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new Lemon.App()); // new in 1.3
+
+        }
+    }
+
+}
+
