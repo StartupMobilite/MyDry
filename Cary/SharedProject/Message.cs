16eb0fca9017243cf3adfa42e2f2248acc3c09a5
bachUp <anne-sophie.bach@outlook.com>
2016-04-26 20:25:33 +0200

    class profile

--- Cary/SharedProject/Message.cs
+++ Cary/SharedProject/Message.cs
@@ -1,46 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Android.Bluetooth;
+using System.IO;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace SharedProject
 {
     class Message
     {
-        public int id { get; set; }
-        public string textMessage { get; set; }
-        public int idPerson { get; set; }
-
-        private static string test = "";
-
-
-        static void sendMessage()
-        {
-            int id = getLastId();
-            saveMessage(id);
-        }
-
-        static void saveMessage(int id)
-        {
-            // You can modify the path if necessary"C:\Users\Public\TestFolder" folder on your machine.
-
-            // Write an array of strings to a file.
-            // Create a string array that consists of three lines.
-            string[] lines = { id.ToString(), "Second line", "Third line" };
-            // WriteAllLines creates a file, writes a collection of strings to the file,
-            // and then closes the file.  You do NOT need to call Flush() or Close().
-            // System.IO.File.WriteAllLines(@"C:\Users\Public\TestFolder\WriteLines.txt", lines);
-
-        }
-
-        public void printMessage()
-        {
-            test = "hello";
-        }
-
-        static int getLastId()
-        {
-            return 3;
-        }
-
+        public int Id { get; set; }
+        public string TextMessage { get; set; }
+        public int IdPerson { get; set; }
     }
 }
---
+++ Cary/SharedProject/Profile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Info user
+
+namespace SharedProject
+{
+    class Profile : UserProfile
+    {
+        
+    }
+
+    public class UserProfile
+    {
+        private string Name { get; set; }
+        private string LastName { get; set; }
+        private string Email { get; set; }
+        private string Weight { get; set; }
+        private string Height { get; set; }
+        private string Old { get; set; }
+        private string Morphology { get; set; }
+    }
+}
